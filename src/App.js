09dbdccf8c0764a05326d9aca83e3841fb7d9241
Richard Walter <richardwalter515@gmail.com>
2021-03-19 19:28:45 -0400

    initial create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Scoreboard from './components/scoreboard';
+
+function App() {
+  return (
+    <div>
+      <Scoreboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/scoreboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Scoreboard() {
+    return (
+        <h1>Let's Keep Score</h1>
+        //how many players? 
+        //depending on how many players, we will generate forms to fill in names for each player.
+        //then for each player we will generate a counter where the user can enter points to add, and when they press submit they will be added to the users total points.
+        //when the game is over, press the game over button to reset to the beginning.
+    )
+}
+
+export default Scoreboard
