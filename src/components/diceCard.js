9506b7ab77974024d9eec48cb1f5e821cb6622b5
Richard Walter <richardwalter515@gmail.com>
2022-01-30 10:59:31 -0500

    dice roll animation

--- src/components/diceCard.js
+++ src/components/diceCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import one from '../assets/dice/dice-one.png';
 import two from '../assets/dice/dice-two.png';
 import three from '../assets/dice/dice-three.png';
@@ -10,6 +10,7 @@ import six from '../assets/dice/dice-six.png';
 function DiceCard() {
     const [diceNumber, setDiceNumber] = useState();
     const [diceImage, setDiceImage] = useState();
+    const [loadingDice, setLoadingDice] = useState();
 
     const imageArr = [one, two, three, four, five, six];
 
@@ -18,16 +19,21 @@ function DiceCard() {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max-min+1) + min);
     }
-
+    
     const diceRoll = () => {
+        setLoadingDice(null);
         let randomNumber = getRandomNumber(1, 6);
         setDiceNumber(randomNumber);
         setDiceImage(imageArr[randomNumber-1]);
     }
+
+    useEffect(() => {
+        setLoadingDice("loading-dice");
+    },[loadingDice])
     
     return (
         <div className="diceCard">
-            <img src={diceImage}></img>
+            <img src={diceImage} className={loadingDice}></img>
             <button onClick={diceRoll}>Roll</button>
         </div>
     )
